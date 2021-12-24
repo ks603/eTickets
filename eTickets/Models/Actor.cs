6ddeaf3b51d057a66e8e4ea1f2a8b8d6e77932f1
Kevin <kevin.shaughnessy18@gmail.com>
2021-12-24 13:08:24 -0500

    Created Relationships and Joint Table for m2m relationship

--- eTickets/Models/Actor.cs
+++ eTickets/Models/Actor.cs
@@ -13,5 +13,8 @@ namespace eTickets.Models
         public string ProfilePictureURL { get; set; }
         public string FullName { get; set; }
         public string Bio { get; set; }
+
+        //Realtionships
+        public List<Actor_Movie> Actor_Movies { get; set; }
     }
 }
