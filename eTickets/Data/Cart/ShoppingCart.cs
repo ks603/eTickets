34ad66f2c704633696da5a4c1f8dad6e969a28f2
Kevin <kevin.shaughnessy18@gmail.com>
2022-01-17 17:53:49 -0500

    Created ShoppingCart class and its functions

---
+++ eTickets/Data/Cart/ShoppingCart.cs
@@ -0,0 +1,33 @@
+﻿using eTickets.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.Cart
+{
+    public class ShoppingCart
+    {
+        public AppDbContext _context { get; set; }
+
+        public string ShoppingCartId { get; set; }
+        public List<ShoppingCartItem> ShoppingCartItems { get; set; }
+        public ShoppingCart(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<ShoppingCartItem> GetShoppingCartItems()
+        {
+            return ShoppingCartItems ?? (ShoppingCartItems = _context.ShoppingCartItems.Where(n => n.ShoppingCartId == ShoppingCartId).Include(n => n.Movie).ToList());
+        }
+
+        public double GetShoppingCartTotal()
+        {
+            var total = _context.ShoppingCartItems.Where(nameof => ShoppingCartId == ShoppingCartId).Select(nameof => nameof.Movie.Price * nameof.Amount).Sum();
+            return total;
+        }
+    }
+}
+
