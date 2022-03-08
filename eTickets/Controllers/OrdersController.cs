f6e54ac7d315d76e30ec44824e59541b1b9e2c4a
Kevin <kevin.shaughnessy18@gmail.com>
2022-03-07 23:04:00 -0500

    You can add and subtract quantities in the shopping cart

--- eTickets/Controllers/OrdersController.cs
+++ eTickets/Controllers/OrdersController.cs
@@ -32,7 +32,7 @@ namespace eTickets.Controllers
             return View(response);
         }
 
-        public async Task<RedirectToActionResult> AddToShoppingCart(int id)
+        public async Task<IActionResult> AddItemToShoppingCart(int id)
         {
             var item = await _moviesService.GetMovieByIdAsync(id);
 
@@ -41,6 +41,17 @@ namespace eTickets.Controllers
                 _shoppingCart.AddItemToCart(item);
             }
             return RedirectToAction(nameof(ShoppingCart)); 
+        }
+
+        public async Task<IActionResult> RemoveItemFromShoppingCart(int id)
+        {
+            var item = await _moviesService.GetMovieByIdAsync(id);
+
+            if (item != null)
+            {
+                _shoppingCart.RemoveItemFromCart(item);
+            }
+            return RedirectToAction(nameof(ShoppingCart));
         }
     }
 }
