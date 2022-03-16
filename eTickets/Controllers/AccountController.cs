80d14518b4e8aae0c7fa2863fbfa532ea98c89d4
Kevin <kevin.shaughnessy18@gmail.com>
2022-03-15 22:39:34 -0400

    You can now sign out users

--- eTickets/Controllers/AccountController.cs
+++ eTickets/Controllers/AccountController.cs
@@ -96,5 +96,12 @@ namespace eTickets.Controllers
             }
             return View("RegisterCompleted");
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Movies");
+        }
     }
 }
